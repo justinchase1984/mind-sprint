5a3b3daa08ebd3d036ba1d4a24284e8e8934f11a
justinchase1984 <justinrosschase@gmail.com>
2025-06-22 12:04:52 +1000

    Update _app.tsx
    Ensure global CSS only imported in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 // pages/_app.tsx
-import type { AppProps } from 'next/app';
-import '../styles/globals.css';
+import type { AppProps } from 'next/app'
+import '../styles/globals.css'   // ← ONLY import your global CSS here
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <Component {...pageProps} />
 }
