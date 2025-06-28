0eefdb9c9f45be51fe17148a3490142010fa709b
justinchase1984 <justinrosschase@gmail.com>
2025-06-28 13:16:33 +1000

    Create _document.tsx
    Add pages/_document.tsx for GA4 tracking

---
+++ pages/_document.tsx
@@ -0,0 +1,35 @@
+// pages/_document.tsx
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* Load the GA4 library */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA4_ID}`}
+          />
+          {/* Initialize GA4 */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GA4_ID}', {
+                  page_path: window.location.pathname,
+                });
+              `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
