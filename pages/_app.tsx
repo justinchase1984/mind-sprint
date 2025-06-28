ff3ed75bbae0beb615b787b687396fbfac30d1e8
justinchase1984 <justinrosschase@gmail.com>
2025-06-28 18:49:10 +1000

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
-// pages/_app.tsx
-import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
 import '../styles/globals.css'
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }) {
+  // Ensure any old preview flags are cleared
+  useEffect(() => {
+    sessionStorage.removeItem('previewDay')
+    localStorage.removeItem('previewDay')
+  }, [])
+
   return <Component {...pageProps} />
 }
