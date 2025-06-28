d425c92ec706bd2f86e3d9fb92095c52b3d45f16
justinchase1984 <justinrosschase@gmail.com>
2025-06-28 18:56:59 +1000

    Update _app.tsx
    Fix implicit any error by typing AppProps

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
-import { useEffect } from 'react'
+// pages/_app.tsx
 import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
 
-export default function App({ Component, pageProps }) {
-  // Ensure any old preview flags are cleared
+export default function MyApp({ Component, pageProps }: AppProps) {
+  // clear any old preview flags on load
   useEffect(() => {
     sessionStorage.removeItem('previewDay')
     localStorage.removeItem('previewDay')
   }, [])
 
+  // render the page
   return <Component {...pageProps} />
 }
