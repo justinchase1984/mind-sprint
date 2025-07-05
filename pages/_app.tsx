03cc5d8fade6dcfd4289880d975672e8b7034c73
justinchase1984 <justinrosschase@gmail.com>
2025-07-05 15:20:29 +1000

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,12 @@
 // pages/_app.tsx
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { useEffect } from 'react'
+import Layout from '../components/Layout'
+import '../styles/globals.css'  // adjust path if your global CSS is elsewhere
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  // clear any old preview flags on load
-  useEffect(() => {
-    sessionStorage.removeItem('previewDay')
-    localStorage.removeItem('previewDay')
-  }, [])
-
-  // render the page
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
