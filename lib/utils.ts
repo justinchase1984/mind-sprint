bdec00d4de18f3857205f4736c3aea40ca2c3e55
justinchase1984 <justinrosschase@gmail.com>
2026-03-15 18:22:28 +1000

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,6 @@
 import type { Puzzle } from './puzzles'
 import { getRotatingPuzzlesByChallenge } from './rotation'
 
-export function getDailyPuzzles(challenge: number): Puzzle[] {
+export function getDailyPuzzles(challenge: number = 1): Puzzle[] {
   return getRotatingPuzzlesByChallenge(challenge)
 }
