cd0d7e0cf7086cc3b1c89349ecce8942456bd711
justinchase1984 <justinrosschase@gmail.com>
2026-03-15 18:04:46 +1000

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,11 +1,6 @@
-Failed to compile.
-./lib/utils.ts:4:3
-Type error: Module '"./puzzles"' has no exported member 'triviaPool'.
-  2 | import type { Puzzle } from './puzzles'
-  3 | import {
-> 4 |   triviaPool,
-    |   ^
-  5 |   scramblePool,
-  6 |   logicPool,
-  7 |   rebusPool,
-Error: Command "npm run build" exited with 1
+import type { Puzzle } from './puzzles'
+import { getRotatingPuzzlesByChallenge } from './rotation'
+
+export function getPuzzlesByChallenge(challenge: number): Puzzle[] {
+  return getRotatingPuzzlesByChallenge(challenge)
+}
