cdb24b07357b69be051e5e7ed0c7205e9a1dcc6e
justinchase1984 <justinrosschase@gmail.com>
2026-03-15 17:55:10 +1000

    Update puzzles.ts

--- lib/puzzles.ts
+++ lib/puzzles.ts
@@ -1,6 +1,5 @@
-lib/puzzles.ts:1:1
-Type error: Cannot find name 'lib'.
-> 1 | lib/puzzles.ts
-    | ^
-  2 |
-Error: Command "npm run build" exited with 1
+export interface Puzzle {
+  question: string
+  answer: string
+  options: string[]
+}
