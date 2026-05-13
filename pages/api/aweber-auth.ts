1703d1e06787f76cedb44981df6142ece2fef3af
justinchase1984 <justinrosschase@gmail.com>
2026-05-13 18:14:39 +1000

    Update aweber-auth.ts

--- pages/api/aweber-auth.ts
+++ pages/api/aweber-auth.ts
@@ -1,1 +1,11 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
 
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const clientId = process.env.AWEBER_CLIENT_ID
+
+  const redirectUri = 'https://dailymindsprint.com/api/aweber-callback'
+
+  const url = `https://auth.aweber.com/oauth2/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}`
+
+  res.redirect(url)
+}
